b7e6c5f6c817372592fba355060965d808e32d59
Francisco Cerdán <fcerdanguerrero@gmail.com>
2023-03-10 01:07:04 +0100

    added simple theme toggle. removed ui from state (#11)

--- src/hooks/useLoadLocalStorage.tsx
+++ src/hooks/useLoadLocalStorage.tsx
@@ -6,21 +6,13 @@ export function useLoadLocalStorage() {
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme') || 'light'
     const savedChats = localStorage.getItem('chats')
-
-    dispatch({
-      type: 'ui/setTheme',
-      payload: savedTheme,
-    })
-
     if (savedChats) {
       dispatch({
         type: 'chats/setChats',
         payload: JSON.parse(savedChats),
       })
     }
-
     setLoaded(true)
   }, [])
 
