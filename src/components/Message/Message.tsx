041413a76103f91399d53ebdcbfe8227c4168cb9
Francisco Cerdán <fcerdanguerrero@gmail.com>
2023-03-18 20:39:09 +0100

    fix: changed SyntaxHighlighter and theme import to proper load in production. Also added copy function (#27)
    closes #26 
    closes #25

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -8,8 +8,13 @@ import robotLight from '@assets/robot-light.svg'
 import robotDark from '@assets/robot-dark.svg'
 
 import ReactMarkdown from 'react-markdown'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import codeBlockTheme from 'react-syntax-highlighter/dist/cjs/styles/hljs/ir-black'
+
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import copyIcon from '@assets/copy.svg'
+import copyGreenIcon from '@assets/copy-green.svg'
+import { useEffect, useState } from 'react'
 
 export enum MessageTestIds {
   Container = 'message-container',
@@ -22,6 +27,16 @@ export type MessageProps = {
 
 const Message = ({ message, idx }: MessageProps) => {
   const currentTheme = useAppSelector(selectTheme)
+
+  const [copied, setCopied] = useState(false)
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false)
+      }, 2500)
+    }
+  }, [copied])
 
   return (
     <div
@@ -43,20 +58,47 @@ const Message = ({ message, idx }: MessageProps) => {
           }
           alt="user"
         />
-        <div>
+        <div className="w-[calc(100%-50px)]">
           <ReactMarkdown
             children={message.content}
             components={{
               code({ node, inline, className, children, ...props }) {
                 const match = /language-(\w+)/.exec(className || '')
                 return !inline && match ? (
-                  <SyntaxHighlighter
-                    children={String(children).replace(/\n$/, '')}
-                    style={coldarkDark as any}
-                    language={match[1]}
-                    PreTag="div"
-                    {...props}
-                  />
+                  <div className="my-4">
+                    <div className="flex items-center text-gray-400 bg-gray-900 py-2 px-4 justify-between ">
+                      <span className="font-bold text-xs ">
+                        {match[1] ? match[1] : ''}
+                      </span>
+                      <CopyToClipboard
+                        text={String(children).replace(/\n$/, '')}
+                        onCopy={() => setCopied(true)}
+                      >
+                        <div className="flex items-center">
+                          <span className="text-s text-green-400">
+                            {copied ? 'Copied!' : ''}
+                          </span>
+                          <button className="bg-gray-900 text-gray-400 p-2 rounded-md mb-1">
+                            <img
+                              src={copied ? copyGreenIcon : copyIcon}
+                              alt="copy"
+                              className="w-4 h-4"
+                            />
+                          </button>
+                        </div>
+                      </CopyToClipboard>
+                    </div>
+                    <div className="overflow-x-auto">
+                      <SyntaxHighlighter
+                        children={String(children).replace(/\n$/, '')}
+                        style={codeBlockTheme as any}
+                        className="!whitespace-pre !px-4 !py-2"
+                        language={match[1]}
+                        PreTag="div"
+                        {...props}
+                      />
+                    </div>
+                  </div>
                 ) : (
                   <code className={className} {...props}>
                     {children}
