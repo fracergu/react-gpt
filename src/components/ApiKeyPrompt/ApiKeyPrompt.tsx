3f2493a29f844c82c2a9012c7b880879f9c3929d
Francisco Cerdán <fcerdanguerrero@gmail.com>
2023-03-26 22:54:46 +0200

    changed style of api key prompt screen

--- src/components/ApiKeyPrompt/ApiKeyPrompt.tsx
+++ src/components/ApiKeyPrompt/ApiKeyPrompt.tsx
@@ -40,31 +40,37 @@ const ApiKeyPrompt = ({ updateApiKey }: ApiKeyPromptProps) => {
       data-testid={ApiKeyPromptTestIds.Container}
       className="flex flex-col w-full h-full items-center justify-center p-3"
     >
-      <div className="flex flex-col gap-3 max-w-[90ch]">
-        <h1 className="text-2xl">No API key found</h1>
+      <div className="flex flex-col gap-3 max-w-[90ch] bg-blue-900 p-6 shadow-lg">
+        <h1 className="text-3xl font-semibold text-gray-100">
+          No API key found
+        </h1>
+        <p className="text-lg text-gray-200">
+          It seems that the application does not have an API key configured in
+          the environment, and a local variable has not been found. Please enter
+          an API key to set it locally.{' '}
+        </p>
         <p>
-          It appears that the application does not have an API key configured in
-          the environment and a local variable has not been found. Please enter
-          an API key to set it locally. You can get it{' '}
+          You can obtain your API key{' '}
           <a
-            className="underline"
+            className="text-blue-300 font-semibold hover:underline"
             href="https://beta.openai.com/account/api-keys"
             rel="noreferrer"
             target="_blank"
           >
-            here.
+            here
           </a>
+          .
         </p>
         <div className="flex gap-3">
           <input
-            className="w-fit focus:outline-none p-2 bg-zinc-700 text-gray-100 rounded-md grow"
+            className="w-fit focus:outline-none p-2 bg-zinc-800 text-gray-300 rounded-none grow"
             type="password"
             placeholder="API key"
             value={apiKey}
             onChange={handleApiKeyInputChange}
           />
           <button
-            className="bg-blue-800 text-white rounded-md p-2 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+            className="bg-blue-700 text-gray-100 rounded-none p-2 focus:outline-none disabled:opacity-50 disabled:bg-zinc-900 disabled:cursor-not-allowed"
             onClick={handleSetApiKeyButtonClick}
             disabled={!isSetButtonEnabled}
           >
@@ -73,7 +79,8 @@ const ApiKeyPrompt = ({ updateApiKey }: ApiKeyPromptProps) => {
         </div>
         {!isApiKeyValid && inputTouched && (
           <p className="text-red-500">
-            Invalid API key: Must have 51 characters and &#39;sk-&#39; prefix
+            Invalid API key: It must have 51 characters and a &#39;sk-&#39;
+            prefix
           </p>
         )}
       </div>
