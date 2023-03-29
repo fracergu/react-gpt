c522eeecf9cfe062b2e2b1ef75d3cf4e7e9090f2
Francisco Cerdán <fcerdanguerrero@gmail.com>
2023-03-29 18:30:34 +0200

    Chore/restore default syntax highligher (#54)
    * restored default syntax highlighter to show highlight in production
    
    * changed code theme

--- src/components/CodeBlock/CodeBlock.tsx
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -1,8 +1,8 @@
 import copyIcon from '@assets/icons/copy.svg'
 import { type ReactNode, useEffect, useState } from 'react'
 import CopyToClipboard from 'react-copy-to-clipboard'
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { irBlack } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { sunburst as highlightTheme } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 export interface CodeBlockProps {
   inline?: boolean
@@ -27,7 +27,7 @@ const CodeBlock = ({ inline = false, match, children }: CodeBlockProps) => {
 
   return !inline ? (
     <div className="my-4 shadow-md" data-testid={CodeBlockTestIds.Container}>
-      <div className="flex items-center text-gray-400 bg-zinc-900 py-2 px-4 justify-between rounded-t-md items-center">
+      <div className="flex items-center text-gray-400 bg-slate-900 py-2 px-4 justify-between rounded-t-md items-center">
         <span className="font-bold text-xs ">
           {match !== null ? match[1] : ''}
         </span>
@@ -49,7 +49,7 @@ const CodeBlock = ({ inline = false, match, children }: CodeBlockProps) => {
       </div>
       <div className="overflow-x-auto rounded-b-md">
         <SyntaxHighlighter
-          style={irBlack}
+          style={highlightTheme}
           className="!whitespace-pre !px-4 !py-2"
           language={match !== null ? match[1] : ''}
           PreTag="div"
@@ -59,7 +59,7 @@ const CodeBlock = ({ inline = false, match, children }: CodeBlockProps) => {
       </div>
     </div>
   ) : (
-    <code className="bg-zinc-900 text-gray-400 py-1 px-2 rounded-md mb-1">
+    <code className="bg-slate-900 text-gray-400 py-1 px-2 rounded-md mb-1">
       {children}
     </code>
   )
